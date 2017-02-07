a7767da47ad1fd4bdb07b149dab2f18ab1e8a4ab
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-02-07 14:55:05 +0200

    JSON data load end-points

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 Route::get('/', 'ApplicationController@index');
 Route::get('/package', 'ApplicationController@package');
+Route::get('/load-venue', 'ApplicationController@loadVenue');
+Route::get('/get-data/{fileName}', 'ApplicationController@getData');
 Route::resource('hotels', 'HotelController');
 Auth::routes();
