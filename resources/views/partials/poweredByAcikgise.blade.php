b082be52c7ea3fa5eb03c2b131cdbca4deab721e
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-02-28 10:38:07 +0200

    Changed powered by acikgise location

--- resources/views/partials/poweredByAcikgise.blade.php
+++ resources/views/partials/poweredByAcikgise.blade.php
@@ -1,5 +1,5 @@
 <div class="poweredBy">
     <a class="poweredBy-toggle" href="http://acikgise.com" target="_blank">
-        <img src="{{ asset('images/poweredbyacikgise.png') }}">
+        <img src="{{ asset('images/poweredbyacikgise.png') }}" width="85px;">
     </a>
 </div>
