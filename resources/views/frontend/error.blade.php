46667cba71259b20347c69018e63688c5d6bd581
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-03-03 14:45:14 +0200

    error page template change

--- resources/views/frontend/error.blade.php
+++ resources/views/frontend/error.blade.php
@@ -5,6 +5,7 @@
 @section('title', 'Error On Purchase!')
 
 @section('content')
+    @include('partials.header')
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
